62e0750301cc9199eee6f5a5bb9c2948afdbf8d3
Mehtab-Ali <rmehtabali@gmail.com>
2023-11-15 23:02:14 -0500

    made a function for variables

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def userInput():
     options = " 1. Add matrices \n 2. Multiply matrix by a constant \n 3. Multiply matrices \n 4. Transpose matrix \n 5. Calculate a determinant \n 6. Inverse Matrix  \n 7. Substract Matrix  \n 0. exit"
     print(options)
     choice = input("What operation would you like to choose : ")
+
     if choice == "1":
         size1 = input("Enter size of first matrix : ")
         matrix = []
@@ -14,12 +15,14 @@ def userInput():
         matrix1 = []
         createMatrix(size2, matrix1)
         addition(size1, size2, matrix, matrix1)
+
     elif choice == "2":
         size1 = input("Enter the size of the matrix : ")
         constant = input("Enter the constant multiplicated : ")
         matrix = []
         createMatrix(size1, matrix)
         constantMultiplication(size1, matrix, constant)
+
     elif choice == "3":
         size1 = input("Enter size of first matrix : ")
         matrix = []
@@ -28,21 +31,25 @@ def userInput():
         matrix1 = []
         createMatrix(size2, matrix1)
         matriceMultiplication(size1, size2, matrix, matrix1)
+
     elif choice == "4":
         size1 = input("Enter the size of the matrix : ")
         matrix = []
         createMatrix(size1, matrix)
         transpose(size1, matrix)
+
     elif choice == "5":
         size1 = input("Enter the size of the matrix : ")
         matrix = []
         createMatrix(size1, matrix)
         determinant(size1, matrix)
+
     elif choice == "6":
         size1 = input("Enter the size of the matrix : ")
         matrix = []
         createMatrix(size1, matrix)
         inverse(size1, matrix)
+
     elif choice == "7":
         size1 = input("Enter size of first matrix : ")
         matrix = []
@@ -51,17 +58,23 @@ def userInput():
         matrix1 = []
         createMatrix(size2, matrix1)
         substract(size1, size2, matrix, matrix1)
+
     else:
         exit()
 
 
-def addition(size1, size2, matrix, matrix1):
+def variables(size1, size2):
     Row = int(size1[0])
     Column = int(size1[2])
 
     Row1 = int(size2[0])
     Column1 = int(size2[2])
 
+    return Row, Column, Column1, Row1
+
+
+def addition(size1, size2, matrix, matrix1):
+    Row, Row1, Column, Column1 = variables(size1, size2)
     if Row != Row1 or Column != Column1:
         print("Cannot add matrices together since it is not the same dimensions")
     else:
@@ -72,11 +85,7 @@ def addition(size1, size2, matrix, matrix1):
 
 
 def substract(size1, size2, matrix, matrix1):
-    Row = int(size1[0])
-    Column = int(size1[2])
-
-    Row1 = int(size2[0])
-    Column1 = int(size2[2])
+    Row, Row1, Column, Column1 = variables(size1, size2)
 
     if Row == Row1 and Column == Column1:
         for row in range(Row):
@@ -88,8 +97,7 @@ def substract(size1, size2, matrix, matrix1):
 
 
 def constantMultiplication(size1, matrix, constant):
-    Row = int(size1[0])
-    Column = int(size1[2])
+    Row, Row1, Column, Column1 = variables(size1, size1)
     Constant = int(constant[0])
 
     for row in range(Row):
@@ -99,8 +107,7 @@ def constantMultiplication(size1, matrix, constant):
 
 
 def transpose(size1, matrix):
-    Row = int(size1[0])
-    Column = int(size1[2])
+    Row, Row1, Column, Column1 = variables(size1, size1)
 
     arr = np.array(matrix)
 
@@ -110,11 +117,7 @@ def transpose(size1, matrix):
 
 
 def matriceMultiplication(size1, size2, matrix, matrix1):
-    Row = int(size1[0])
-    Column = int(size1[2])
-
-    Row1 = int(size2[0])
-    Column1 = int(size2[2])
+    Row, Row1, Column, Column1 = variables(size1, size2)
 
     if Column != Row1:
         print("The operation cannot be performed.")
@@ -137,8 +140,7 @@ def matriceMultiplication(size1, size2, matrix, matrix1):
 
 
 def determinant(size1, matrix):
-    Row = int(size1[0])
-    Column = int(size1[2])
+    Row, Row1, Column, Column1 = variables(size1, size1)
 
     if Row != Column:
         ("The determinant cannot be calculated")
@@ -152,8 +154,7 @@ def determinant(size1, matrix):
 
 
 def createMatrix(size1, matrix):
-    Row = int(size1[0])
-    Column = int(size1[2])
+    Row, Row1, Column, Column1 = variables(size1, size1)
 
     # Initialize matrix
 
@@ -172,9 +173,6 @@ def createMatrix(size1, matrix):
 
 
 def inverse(size1, matrix):
-    Row = int(size1[0])
-    Column = int(size1[2])
-
     arr = np.array(matrix)
     inverse = np.linalg.inv(arr)
 
